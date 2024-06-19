cb549a8027af914f73848080033f154e54427c4a
dinesh-gaire <gairedinesh132@gmail.com>
2024-06-19 17:05:39 +0545

    add user functionality created

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,6 +25,17 @@ var MyApp = (function(){
             addUser(data.other_user_id, data.connId);
         })
     }
+    // add User and set connection
+    function addUser(other_user_id, connId){
+        var newDivId = $("#otherTemplate").clone();
+        newDivId = newDivId.attr("id", connId).addClass("other");
+        newDivId.find("h2").text(other_user_id);
+        newDivId.find("video").attr("id", "v_"+connId);
+        newDivId.find("audio").attr("id", "a_"+connId);
+        newDivId.show();
+        $("#divUsers").append(newDivId);       
+
+    }
 
     return {
         _init: function(uid, mid){
