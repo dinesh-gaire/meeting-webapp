c1f2fa02d259bd69db9ee32b3b6b074dcdef98a0
dinesh-gaire <gairedinesh132@gmail.com>
2024-06-19 16:48:37 +0545

    functionality to know other users added

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ var server = app.listen(3000, function(){
     console.log("Listening on port 3000");
 });
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server,{allowEIO3: true});
 app.use(express.static(path.join(__dirname,"")));
 
+var userConnections = [];
 io.on("connection", (socket)=>{
     console.log("Socket id is", socket.id);
+    socket.on("userconnect", (data)=>{
+        console.log("userconnect", data.displayName, data.meetingId);
+
+        var other_users = userConnections.filter((p)=> p.meetingId==data.meetingId)
+        userConnections.push({
+            connectionId: socket.id,
+            user_id: data.displayName,
+            meeting_id: data.meetingId
+        })
+
+        other_users.forEach((v)=>{
+            socket.to(v.connectionId).emit("inform_others_about_me", {
+                other_user_id: data.displayName,
+                connId: socket.id
+            })
+        })
+    })
 })
 
